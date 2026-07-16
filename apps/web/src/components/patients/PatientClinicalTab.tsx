b215a418858b0f3802493a6fb5130047131c76ce
marko1olo <marko1olo@users.noreply.github.com>
2026-07-16 22:32:05 +0400

    refactor: decompose PatientsView into subcomponent tabs under components/patients

---
+++ apps/web/src/components/patients/PatientClinicalTab.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { usePatientStore } from "../../store/patientStore";
+import { Dashboard } from "@dental/shared";
+import { AnamnesisPanel } from "../AnamnesisPanel";
+import { OdontogramModule } from "../odontogram/OdontogramModule";
+import { VisiographAnalyzer } from "../imaging/VisiographAnalyzer";
+
+import { PatientsViewProps } from "../../PatientsView";
+export function PatientClinicalTab({ props }: { props: PatientsViewProps }) {
+    const { selectedPatientId } = usePatientStore();
+
+    return (
+        <>
+						{/* Premium Clinical Experience (Full Width Odontogram + Grid) */}
+						<div className="patients-flex-col-gap-24-my">
+							{selectedPatientId && (
+								<AnamnesisPanel patientId={selectedPatientId} />
+							)}
+							<div className="patients-w-100">
+								{selectedPatientId && (
+									<OdontogramModule
+										patientId={selectedPatientId}
+										pediatricMode={Boolean((props.dashboard?.clinicSettings?.profile as any)?.hasPediatricMode)}
+									/>
+								)}
+							</div>
+
+							<div className="patient-clinical-grid patients-my-0">
+								<div className="clinical-col-left">
+									<VisiographAnalyzer />
+								</div>
+								<div className="clinical-col-right">
+									
+									
+								</div>
+							</div>
+						</div>
+        </>
+    );
+}
