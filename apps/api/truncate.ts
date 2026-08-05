8dba4744ca6f92f03cd30e3153895f0c90eb05cb
marko1olo <marko1olo@users.noreply.github.com>
2026-08-05 22:03:46 +0400

    chore(api): remove obsolete root scratch migration scripts

--- apps/api/truncate.ts
+++
@@ -1,8 +0,0 @@
-import { PGlite } from "@electric-sql/pglite";
-
-async function run() {
-	const db = new PGlite("./dente-db");
-	await db.query("TRUNCATE TABLE payments CASCADE;");
-	process.exit(0);
-}
-run();
