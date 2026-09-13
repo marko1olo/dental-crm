62758963dc6fe66a958ddcd48bb42783b4f2bdc2
marko1olo <marko1olo@users.noreply.github.com>
2026-09-13 15:31:02 +0400

    refactor(shared): consolidate FNS tax deduction engine into SSOT (Mandate 8s)

--- packages/shared/src/fiscal/index.ts
+++ packages/shared/src/fiscal/index.ts
@@ -12,7 +12,6 @@ export * from "./offlineFiscalBatchReconciler.js";
 export * from "./sbpQrEngine.js";
 export * from "./familyFiscalBillingEngine.js";
 export * from "./qrGenerator.js";
-export * from "../finance/taxDeduction.js";
 export * from "./fnsTaxDeductionEngine.js";
 export * from "./barcodeGenerator.js";
 export * from "./familyTaxBatchEngine.js";
